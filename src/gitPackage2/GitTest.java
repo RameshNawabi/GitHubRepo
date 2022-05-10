586c135106e60aed5fdc6561f7a9d449f27f1e6c
RameshNawabi <fawad.nawabbi@gmail.com>
2022-05-09 23:26:02 -0700

    minor changes

--- src/gitPackage2/GitTest.java
+++ src/gitPackage2/GitTest.java
@@ -3,8 +3,14 @@ package gitPackage2;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int a = 10;
+		int b = 20;
+		
+		System.out.println(a+b);
+		
+		
+		
 	}
 
 }

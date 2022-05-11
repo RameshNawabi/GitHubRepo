03a43171544b94a736482d1ddc6444c280675156
RameshNawabi <fawad.nawabbi@gmail.com>
2022-05-10 23:58:59 -0700

    some codes has been changed recently

--- src/gitPackage2/GitTest.java
+++ src/gitPackage2/GitTest.java
@@ -4,14 +4,17 @@ public class GitTest {
 
 	public static void main(String[] args) {
 
-		int a = 10;
-		int b = 20;
+		int a = 20;
+		int b = 10;
 		
 		System.out.println(a-b);
 		// this is the changes form remote repository
 		
 		String r = "Ramesh";
 		System.out.println(r);
+		
+		StringBuilder builder = new StringBuilder(r);
+		System.out.println(builder.reverse());
 		
 		
 	}
